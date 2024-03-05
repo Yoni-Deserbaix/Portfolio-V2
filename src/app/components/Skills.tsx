b45108cb743f1e88a7791bfbac5a380aed174b5e
unknown <yonideserbaix@gmail.com>
2024-03-05 23:33:44 +0100

    update style

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -16,7 +16,7 @@ export const Skills = () => {
         </h5>
       </div>
       <div className="flex flex-col  items-center w-full mt-20 gap-28 max-xl:gap-14 max-lg:mt-10">
-        <div className="flex flex-col gap-7 items-center ml-96 max-lg:ml-0">
+        <div className="flex flex-col gap-7 items-center max-lg:ml-0">
           <h5 className="text-xl font-bold">FRONTEND</h5>
           <div className="rounded-2xl w-[800px] py-2.5 px-10 bg-gradient-to-b from-secondary to-third shadow-xl max-xl:w-full">
             <div className="flex gap-8 w-full max-[400px]:gap-4">
@@ -66,7 +66,7 @@ export const Skills = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-7 items-center mr-96 max-lg:mr-0">
+        <div className="flex flex-col gap-7 items-center max-lg:mr-0">
           <h4 className="text-xl font-bold">BACKEND</h4>
           <div className="rounded-2xl w-[800px] py-2.5 px-10 bg-gradient-to-b from-secondary to-third shadow-xl max-xl:w-full">
             <div className="flex gap-8 w-full max-[400px]:gap-4">
@@ -98,7 +98,7 @@ export const Skills = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-7 items-center ml-96 max-lg:ml-0">
+        <div className="flex flex-col gap-7 items-center  max-lg:ml-0">
           <h4 className="text-xl font-bold">Outils</h4>
           <div className="rounded-2xl w-[800px] py-2.5 px-10 bg-gradient-to-b from-secondary to-third shadow-xl max-xl:w-full">
             <div className="flex gap-8 w-full max-[400px]:gap-4">
