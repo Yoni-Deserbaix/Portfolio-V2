9472073918f9de47d4f108b01eb0ab1355a1d200
unknown <yonideserbaix@gmail.com>
2024-03-05 16:53:28 +0100

    update footer style

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,21 +1,21 @@
 export default function () {
   return (
-    <footer className="footer">
-      <p> Copyright © 2024 • Make with ❤️ By Yoni Deserbaix</p>
-      <div className="socials">
+    <footer className="flex bg-[#2d2e32] items-center flex-row justify-evenly px-2.5 py-[50px]">
+      <p className="text-sm text-[whitesmoke] mb-[15px]"> Copyright © 2024 • Make with ❤️ By Yoni Deserbaix</p>
+      <div className="socials ">
         <a
           href="http://linkedin.com/in/yoni-deserbaix"
           target="_blank"
           rel="noreferrer"
         >
-          <i className="bx bxl-linkedin-square"></i>
+          <i className="bx bxl-linkedin-square text-[1.8rem] text-[whitesmoke] transition-all duration-[0.2s] mr-[15px] hover:opacity-[70%]"></i>
         </a>
         <a
           href="https://github.com/Yoni-Deserbaix"
           target="_blank"
           rel="noreferrer"
         >
-          <i className="bx bxl-github"></i>
+          <i className="bx bxl-github text-[1.8rem] text-[whitesmoke] transition-all duration-[0.2s] mr-[15px] hover:opacity-[70%]"></i>
         </a>
       </div>
     </footer>
