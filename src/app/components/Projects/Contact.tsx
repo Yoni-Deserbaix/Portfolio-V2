0f3736b620a8ffae769728604ae10c8e727b0eee
unknown <yonideserbaix@gmail.com>
2024-03-05 16:34:23 +0100

    add contact section

---
+++ src/app/components/Projects/Contact.tsx
@@ -0,0 +1,53 @@
+export default function Contact() {
+  return (
+    <section className="contact" id="contact">
+      <div className="container">
+        <div className="contact-content">
+          <h1 id="contact-title" data-aos="fade-up" data-aos-duration="300">
+            Contact
+          </h1>
+          <p
+            id="contact-hook-sentence"
+            data-aos="fade-up"
+            data-aos-delay="300"
+            data-aos-duration="300"
+          >
+            Une opportunité d'alternance ? Contactez-moi ! 👇
+          </p>
+          <div
+            className="contact-wrapper"
+            data-aos="fade-up"
+            data-aos-delay="500"
+            data-aos-duration="300"
+          >
+            <div className="contact-container">
+              <span>
+                <i className="bx bxs-map"></i>{" "}
+              </span>
+              <div className="contact-box">
+                <h3>Lieu</h3>
+                <p>Nantes, France</p>
+              </div>
+            </div>
+            <div className="contact-container">
+              <span>
+                <i className="bx bxs-envelope"></i>{" "}
+              </span>
+              <div className="contact-box">
+                <h3>Mail</h3>
+                <a
+                  href="mailto:yonideserbaix@gmail.com"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="social-button"
+                >
+                  yonideserbaix@gmail.com
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Contact from "./components/Projects/Contact";
 import Projects from "./components/Projects/Projects";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
         <Hero />
         <About />
         <Projects />
+        <Contact />
       </main>
     </div>
   );
