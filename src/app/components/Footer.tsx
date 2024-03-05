c75e7bf1e126806754f185057e6fe1b357bac860
unknown <yonideserbaix@gmail.com>
2024-03-05 16:50:25 +0100

    add footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,23 @@
+export default function () {
+  return (
+    <section className="footer">
+      <p> Copyright © 2024 • Make with ❤️ By Yoni Deserbaix</p>
+      <div className="socials">
+        <a
+          href="http://linkedin.com/in/yoni-deserbaix"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="bx bxl-linkedin-square"></i>
+        </a>
+        <a
+          href="https://github.com/Yoni-Deserbaix"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="bx bxl-github"></i>
+        </a>
+      </div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <About />
         <Projects />
         <Contact />
+        <Footer />
       </main>
     </div>
   );
