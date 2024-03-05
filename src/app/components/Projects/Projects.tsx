173d6c2f7e9e1c694c3bc95121d18dd3a73cd971
unknown <yonideserbaix@gmail.com>
2024-03-05 12:56:24 +0100

    add projects section

---
+++ src/app/components/Projects/Projects.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function () {
+  return (
+    <section className="work-section  w-3/5 m-auto px-8 py-[100px]" id="projects">
+      <h3 className="section-title text-[1.3rem] font-extrabold pb-8" data-aos="fade-up" data-aos-duration="300">
+        Projets
+      </h3>
+      <div
+        className="subtitle"
+        data-aos="fade-up"
+        data-aos-delay="500"
+        data-aos-duration="300"
+      >
+        <span className="section-subtitle text-[#767676] pt-80">
+          Parce-que <b>quelques images</b> valent mieux que{" "}
+          <b>10 000 lignes de code</b>, je vous laisse découvrir ci-dessous
+          quelques{""}
+          <b>projets</b> que j'ai pu réaliser !
+        </span>
+      </div>
+
+      {/* <Works /> */}
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects/Projects";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <main>
         <Hero />
         <About />
+        <Projects />
       </main>
     </div>
   );
