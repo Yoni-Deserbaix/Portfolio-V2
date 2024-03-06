1aec7e8184b30ed762b52cf14c5ffed3fcefd5ab
unknown <yonideserbaix@gmail.com>
2024-03-06 10:58:01 +0100

    update export default function

--- src/app/components/Skills/Skills.tsx
+++ src/app/components/Skills/Skills.tsx
@@ -1,10 +1,8 @@
-"use client";
-
 import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import taiwindlogo from "../assets//languages/tailwindcss-color.svg";
-export const Skills = () => {
+import taiwindlogo from "../../assets//languages/tailwindcss-color.svg";
+export default function Skills() {
   return (
     <section
       id="skills"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import { Skills } from "./components/Skills";
+import Skills from "./components/Skills/Skills";
 
 export default function Home() {
   return (
