cc87f374aaae7dba935e1124e32dbd877f4faf05
unknown <yonideserbaix@gmail.com>
2024-03-06 14:18:34 +0100

    update style

--- src/app/components/Skills/Skills.tsx
+++ src/app/components/Skills/Skills.tsx
@@ -7,7 +7,7 @@ export default function Skills() {
   return (
     <section
       id="skills"
-      className="relative h-full pt-20 pb-14 max-lg:pt-14 max-lg:pb-0 bg-[whitesmoke]"
+      className="relative h-full pt-28 pb-14 max-lg:pt-14 max-lg:pb-0 bg-[whitesmoke]"
     >
       <div className="flex justify-center items-center">
         <h5 className="text-xl font-extrabold">COMPÉTENCES</h5>
@@ -15,7 +15,7 @@ export default function Skills() {
       <div className="flex flex-col  items-center w-full mt-20 gap-28 max-xl:gap-14 max-lg:mt-10">
         <div className="flex flex-col gap-7 items-center max-lg:ml-0">
           <h5 className="text-xl font-bold">FRONTEND</h5>
-          <div className="rounded-2xl w-[800px] py-2.5 px-10 bg-gradient-to-b from-secondary to-third shadow-xl max-xl:w-full">
+          <div className="rounded-2xl w-[800px] py-4 px-10 bg-gradient-to-b from-secondary to-third shadow-xl max-xl:w-full">
             <div className="flex flex-wrap gap-8 justify-evenly">
               {skillsData
                 .filter((language) => language.category === "Frontend")
@@ -39,7 +39,7 @@ export default function Skills() {
         </div>
         <div className="flex flex-col gap-7 items-center max-lg:mr-0">
           <h4 className="text-xl font-bold">BACKEND</h4>
-          <div className="rounded-2xl w-[800px] py-2.5 px-10 bg-gradient-to-b from-secondary to-third shadow-xl max-xl:w-full">
+          <div className="rounded-2xl w-[800px] py-4 px-10 bg-gradient-to-b from-secondary to-third shadow-xl max-xl:w-full">
             <div className="flex flex-wrap gap-8 justify-evenly">
               {skillsData
                 .filter((language) => language.category === "Backend")
@@ -63,7 +63,7 @@ export default function Skills() {
         </div>
         <div className="flex flex-col gap-7 items-center  max-lg:ml-0">
           <h4 className="text-xl font-bold">Outils</h4>
-          <div className="rounded-2xl w-[800px] py-2.5 px-10 bg-gradient-to-b from-secondary to-third shadow-xl max-xl:w-full">
+          <div className="rounded-2xl w-[800px] py-4 px-10 bg-gradient-to-b from-secondary to-third shadow-xl max-xl:w-full">
             <div className="flex flex-wrap gap-8 justify-evenly">
               {skillsData
                 .filter((language) => language.category === "Tools")
