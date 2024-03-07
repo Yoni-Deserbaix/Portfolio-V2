b1bad9bce7eb2b326d437781585927783d041118
unknown <yonideserbaix@gmail.com>
2024-03-07 17:22:33 +0100

    update height's footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function () {
   return (
-    <footer className="flex bg-[#2d2e32] items-center flex-row justify-evenly px-2.5 py-[50px]">
+    <footer className="flex bg-[#2d2e32] items-center flex-row justify-evenly px-2.5 py-16">
       <p className="text-base text-[whitesmoke] mb-[15px] max-lg:text-sm max-lg:pl-4 max-lg:w-3/4">
         Copyright © 2024 • Make with ❤️ By Yoni Deserbaix
       </p>
