437202379be382338a84827e13d35e5f56094149
unknown <yonideserbaix@gmail.com>
2024-03-23 10:51:15 +0100

    few style update

--- src/app/components/Skills/Skills.tsx
+++ src/app/components/Skills/Skills.tsx
@@ -6,7 +6,7 @@ export default function Skills() {
   return (
     <section
       id="skills"
-      className="relative h-full pt-28 pb-14 max-lg:pt-14 max-lg:pb-0 max-md:py-24 bg-[whitesmoke]"
+      className="relative h-full pt-28 pb-14 max-lg:pt-14  max-md:py-24 bg-[whitesmoke]"
     >
       <div className="container mt-4 w-4/5 m-auto">
         <h3 className="text-xl font-extrabold">COMPÉTENCES</h3>
@@ -14,7 +14,7 @@ export default function Skills() {
       <div className="flex flex-col items-center w-full mt-20 gap-28 max-xl:gap-14 max-lg:mt-10">
         {["Frontend", "Backend", "Tools"].map((category, index) => (
           <div className="flex flex-col gap-7 items-center" key={index}>
-            <h4 className="text-xl font-bold aos-init">{category}</h4>
+            <h4 className="text-xl font-bold">{category}</h4>
             <div className="rounded-2xl w-[800px] py-4 px-10 shadow-[0_0_10px_rgba(0,0,0,0.1)] max-xl:w-full">
               <div className="flex flex-wrap gap-8 justify-evenly">
                 {skillsData
